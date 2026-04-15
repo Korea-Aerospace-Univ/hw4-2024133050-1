df50ee2e8abbe83bdd713699c6a56d1ab2fb4899
2024133050 <dh262777@gmail.com>
2026-04-15 16:56:57 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -6,29 +6,34 @@ int main()
     char ch=0;
     int cur_letter =0, max_letter=0;
     int cur_number=0, max_number=0;
+
     scanf("%d",&N);
+
     for(int i=1;i<=N;i++){
         scanf(" %c",&ch);
         
-        if (ch>='a'&& ch<='z'){
-            cur_letter ++;
+        if (ch>='a' && ch<='z'){
+            cur_letter++;
             if(cur_letter > max_letter)
                 max_letter = cur_letter;
+
+            cur_number = 0;
         }
-        else 
-            cur_letter=0;
-    
-        if(ch>='0' && ch<='9'){
-            cur_number ++;
+        else if(ch>='0' && ch<='9'){
+            cur_number++;
             if(cur_number > max_number)
                 max_number = cur_number;
+
+            cur_letter = 0;
         }
-        
-        else
-            cur_number=0;
-        
+        else {
+            cur_letter = 0;
+            cur_number = 0;
+        }
     }
-    
+
     printf("%d\n",max_letter);
     printf("%d\n",max_number);
+
+    return 0;
 }

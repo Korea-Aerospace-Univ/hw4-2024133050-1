92544f325bc294088433c723580cbab1fd866d72
2024133050 <dh262777@gmail.com>
2026-04-15 17:32:48 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,23 +2,24 @@
 
 int main()
 {
-    int answer=0,n=0,sum=1;
-    scanf("%d",&answer);
-    
+    int answer = 0, n = 0, sum = 1; //정답,정수,횟수 초기화
+    scanf("%d", &answer);      //정답 입력받기
+    //do while 문 조건(정수가 정답이 아닐때 반복)
     do {
-        
-        scanf("%d",&n);
-        if(n > answer)
-            printf("%d>?\n",n);
-        else if(n<answer)
-            printf("%d<?\n",n);
-        else{
-            printf("%d==?\n",n);
-            printf("%d",sum);
-            break;
+        scanf("%d", &n);    //정수 입력받기
+
+        if (n > answer)    //정수가 정답보다 크면 정수>? 출력
+            printf("%d>?\n", n);
+        else if (n < answer) //정수가 정답보다 작으면 정수<? 출력
+            printf("%d<?\n", n);
+        else {                //정수가 정답인경우 정수== 출력하고 횟수 출력
+            printf("%d==?\n", n);
+            printf("%d\n", sum);
         }
-        sum ++;
-    }while(n);
-    
+
+        sum++;            //횟수 카운트
+
+    } while (n != answer);   //정수가 정답이 아닐경우 반복
+
     return 0;
 }
